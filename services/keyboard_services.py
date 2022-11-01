98c0965b19568c51e7e791e902fc39bd06744b67
Alasa <ogboanohrichardjamb2020@gmail.com>
2022-11-01 19:33:04 +0100

    Made changes on the keyboard_service.py

--- services/keyboard_services.py
+++ services/keyboard_services.py
@@ -1,9 +1,10 @@
 import pyray
 from shared.point import Point
 
+
 class KeyboardService:
     """Detects player input. 
-    
+
     The responsibility of a KeyboardService is to detect player key presses and translate them into 
     a point representing a direction.
 
@@ -11,9 +12,9 @@ class KeyboardService:
         cell_size (int): For scaling directional input to a grid.
     """
 
-    def __init__(self, cell_size = 1):
+    def __init__(self, cell_size=1):
         """Constructs a new KeyboardService using the specified cell size.
-        
+
         Args:
             cell_size (int): The size of a cell in the display grid.
         """
@@ -25,22 +26,15 @@ class KeyboardService:
         Returns:
             Point: The selected direction.
         """
-        dx = 0
-        dy = 0
+        x = 0
 
         if pyray.is_key_down(pyray.KEY_LEFT):
-            dx = -1
-        
-        if pyray.is_key_down(pyray.KEY_RIGHT):
-            dx = 1
-        
-        if pyray.is_key_down(pyray.KEY_UP):
-            dy = -1
-        
-        if pyray.is_key_down(pyray.KEY_DOWN):
-            dy = 1
+            x = -1
 
-        direction = Point(dx, dy)
+        if pyray.is_key_down(pyray.KEY_RIGHT):
+            x = 1
+
+        direction = Point(x, 0)
         direction = direction.scale(self._cell_size)
-        
+
         return direction
